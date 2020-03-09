bdccc27de8bee6c4792d932d13fc241a5ae667ff
PAWEŁ KARAS <pwkaras@poczta.onet.pl>
2020-03-09 22:04:18 +0100

    quiz

---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+function quizF() {
+    const cake = document.getElementById("answer").innerHTML = `<p>prawie dobrze</p><h2><i class="fas fa-thumbs-down"></i></h2><p>spróbuj jeszcze raz</p>`
+    }           
+
+    function quizT() {
+        const cake = document.getElementById("answer").innerHTML = `<p>SUPER</p><h2><i class="fas fa-thumbs-up"></i></h2><img src="images/gabriel-gabriel-4HXOzHQInPw-unsplash.jpg" alt="fly"><p>to niestety prawda</p>`
+        }           
